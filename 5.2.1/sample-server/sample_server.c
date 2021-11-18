1e0aac3a1f8c91e6a228153beee805024b87a37d
Yuya Makiyama <mackyym120307@gmail.com>
2021-11-18 15:50:12 +0900

    lint

--- 5.2.1/sample-server/sample_server.c
+++ 5.2.1/sample-server/sample_server.c
@@ -7,7 +7,7 @@
 #include        <stdio.h>
 #include        <stdlib.h>
 #include        <string.h>
-#include	<unistd.h>
+#include        <unistd.h>
 
 #define  SERVER_PORT     5001   /* 着信側の待ち受けポート番号 */
 #define  MAXPKTLEN       1004   /* 最大パケット長(バイト)     */
@@ -18,18 +18,18 @@ int main(int argc, char **argv)
 	struct sockaddr_in	servaddr, cliaddr; /*  必要な変数    　*/
 	socklen_t len;
 	
-        char recvpkt[MAXPKTLEN];    /* 受信パケット全体を格納する配列変数       */
-        char recvdata[MAXPKTLEN-4]; /* 受信パケットのデータ部を格納する配列変数 */
-        char sendpkt[4];            /* ACK パケット全体を格納する4バイトの配列変数  */ 
-        int seq_num = 0; 
-        int num_bytes_pkt = 0;
-        int num_bytes_data = 0;
-        int total_num_bytes_data = 0;
+  char recvpkt[MAXPKTLEN];    /* 受信パケット全体を格納する配列変数       */
+  char recvdata[MAXPKTLEN-4]; /* 受信パケットのデータ部を格納する配列変数 */
+  char sendpkt[4];            /* ACK パケット全体を格納する4バイトの配列変数  */ 
+  int seq_num = 0; 
+  int num_bytes_pkt = 0;
+  int num_bytes_data = 0;
+  int total_num_bytes_data = 0;
 
-        FILE *fp;
-        char filename[50];
+  FILE *fp;
+  char filename[50];
 
-        /* UDPのソケットを設定 */
+  /* UDPのソケットを設定 */
         
 	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
 
@@ -39,62 +39,60 @@ int main(int argc, char **argv)
 	servaddr.sin_port        = htons(SERVER_PORT);
 	len			 = sizeof(cliaddr);
 
-	if (bind(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0)
-	{
+	if (bind(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0) {
 		printf("Bind error\n");
 		return -1;
 	}
 
 	/* 受信パケットのデータ部を書き出すファイルをオープン */
+  fp = fopen("test.jpg","wb");
 
-        fp = fopen("test.jpg","wb");
+  if (fp == NULL) {
+    printf("File open error: test.jpg\n");
+    return -1;
+  }
+  
+  while(1) {
+    /* 相手からパケットを１つ受信して全体をrecvpktに格納 */
+    num_bytes_pkt
+      = recvfrom(sockfd, recvpkt, MAXPKTLEN, 0, 
+          (struct sockaddr *)&cliaddr, &len);
 
-        if (fp == NULL)
-	{
-	  printf("File open error: test.jpg\n");
-	  return -1;
-        }
-        
-        while(1) {
-		/* 相手からパケットを１つ受信して全体をrecvpktに格納 */
-		num_bytes_pkt
-			 = recvfrom(sockfd, recvpkt, MAXPKTLEN, 0, 
-					(struct sockaddr *)&cliaddr, &len);
+    /* 受信パケット長 num_bytes_pkt - 4 が パケットのデータ部の長さ */
+    num_bytes_data = num_bytes_pkt - 4;
+    total_num_bytes_data += num_bytes_data;
 
-		/* 受信パケット長 num_bytes_pkt - 4 が パケットのデータ部の長さ */
-		num_bytes_data = num_bytes_pkt - 4;
-		total_num_bytes_data += num_bytes_data;
+    /* 受信パケットの先頭4バイト(ヘッダ部)を変数seq_numにコピー */
+    memcpy((char *)&seq_num,recvpkt,4); 
 
-		/* 受信パケットの先頭4バイト(ヘッダ部)を変数seq_numにコピー */
-		memcpy((char *)&seq_num,recvpkt,4); 
+    /* 受信パケットの先頭から5バイト目以降末尾まで(データ部)を  */
+    /* recvdataにコピー                                         */
+    memcpy(recvdata,recvpkt+4,num_bytes_data); 
+    printf("Receive : %d bytes, seq_num=%d\n",num_bytes_data,seq_num);
 
-		/* 受信パケットの先頭から5バイト目以降末尾まで(データ部)を  */
-		/* recvdataにコピー                                         */
-		memcpy(recvdata,recvpkt+4,num_bytes_data); 
-		printf("Receive : %d bytes, seq_num=%d\n",num_bytes_data,seq_num);
+    /* sendpkt(長さ4バイト)にseq_num(長さ4バイト)の内容をコピー */
+    memcpy(sendpkt,(char *)&seq_num,4);
 
-		/* sendpkt(長さ4バイト)にseq_num(長さ4バイト)の内容をコピー */
-		memcpy(sendpkt,(char *)&seq_num,4);
+    /* sendpktの内容をACKパケットとして相手に送信 */
+    sendto(sockfd, sendpkt, 4, 0, 
+          (struct sockaddr *)&cliaddr, 
+          sizeof(cliaddr));
+    printf("ACK with seq_num %d is sent : \n",seq_num);
 
-		/* sendpktの内容をACKパケットとして相手に送信 */
-		sendto(sockfd, sendpkt, 4, 0, 
-					(struct sockaddr *)&cliaddr, 
-					sizeof(cliaddr));
-		printf("ACK with seq_num %d is sent : \n",seq_num);
-		
-		/* recvdata の内容を ファイルに書き出し */
-		fwrite(recvdata,num_bytes_data,1,fp);
-		
-		if (seq_num == 0) {
-			break;
-		}
-	}
-        fclose(fp);
-        close(sockfd);
+    /* recvdata の内容を ファイルに書き出し */
+    fwrite(recvdata,num_bytes_data,1,fp);
+
+    if (seq_num == 0) {
+      break;
+    }
+  }
+
+  fclose(fp);
+  close(sockfd);
 
 	printf("Total amount of received data : \
 				%d bytes\n",total_num_bytes_data);
 
-        return 0;
+  return 0;
 }
 
